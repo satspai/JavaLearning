d462c561a0da3a509e259467d76d0f761d2c0a65
Satish Pai <hisatishpai@gmail.com>
2024-09-07 19:47:37 +0100

    first commit

---
+++ src/main/java/org/example/assignmentLoop.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class assignmentLoop {
+
+        public static void main(String[] args) {
+            /*
+            for (int i=0;i<5;i++) {
+                System.out.println("hello world");
+            }
+
+             */
+            /*
+            int i=0;
+            while (i<=10){
+                System.out.println(i);
+                i++;
+            }
+
+             */
+            /*
+            int i=0;
+            do {
+                System.out.println(i);
+                i++;
+            }
+            while (i<=10);
+             */
+            int a=10;
+            int b=a++;
+            System.out.println(a);
+            System.out.println(b);
+
+        }
+
+
+}
---
+++ src/main/java/org/example/overloading.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class overloading {
+    public static void main(String[] args) {
+        calc(2,4);
+        calc(1,2,3);
+    }
+    public static void calc(int a,int b){
+        System.out.println(a+b);
+    }
+    public static void calc(int a,int b,int c){
+        System.out.println(a+b+c);
+    }
+}
